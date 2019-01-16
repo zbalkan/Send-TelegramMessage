18c48a724c32064561a39291d17400a2f29e8e2f
Zafer Balkan <zafer@zaferbalkan.com>
2019-01-16 10:42:43 +0300

    Whitespace

--- Send-TelegramMessage.ps1
+++ Send-TelegramMessage.ps1
@@ -1,16 +1,16 @@
-﻿<#
+<#
 .Synopsis
    Send text messages using Telegram API
 .DESCRIPTION
    It's a helpful source for system administrators to create Telegram messages using PowerShell. It's a good tool 
    when monitoring solution does not provide integration or when there is not an advanced monitoring tool needed.
 .RELATED LINKS
-    https://github.com/zbalkan/Send-TelegramMessage
+   https://github.com/zbalkan/Send-TelegramMessage
 .EXAMPLE
    Send-TelegramMessage -Message "Hello World"
 .EXAMPLE
    "Hello World" | Send-TelegramMessage
-   #>
+  #>
    function Send-TelegramMessage {
     [CmdletBinding()]
     param (
@@ -69,7 +69,7 @@
 
             # Send message to User
             if($null -eq $TLPeer) {
-            
+
                 # Log the event
                 $TLLogMessage = "$(Get-Date -Format o)`t|`tWARNING`t|`tPeer not found."
                 Write-Warning "Peer not found."
